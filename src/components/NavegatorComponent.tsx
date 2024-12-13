64554e1ab896ef8b79d3102cb88368449386addc
Diego Yance <diego.yance.arqque@gmail.com>
2024-12-13 10:43:16 -0500

    Fix language menu in navegator

--- src/components/NavegatorComponent.tsx
+++ src/components/NavegatorComponent.tsx
@@ -11,7 +11,7 @@ import {
     faBars
 } from '@fortawesome/free-solid-svg-icons'
 import { NavegatorProps } from '../props';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 function NavegatorComponent({ preference, translationLiteral, changeLang, changeThemeMode }: NavegatorProps) {
     let isDark = preference?.dark;
@@ -20,10 +20,23 @@ function NavegatorComponent({ preference, translationLiteral, changeLang, change
 
     //State
     const [openLang, setOpenLang] = useState(false)
-    const [openMenu, setOpenMenu] = useState(true)
+    const [openMenu, setOpenMenu] = useState(false)
+
+    const itemLangRef = useRef<HTMLLIElement  | null>(null);
+    const menuFloatRef = useRef<HTMLDivElement  | null>(null);
 
     const toggleLang = () => {
-        setOpenLang(open => !open);
+        setOpenLang(open => {
+            if(!open) {
+                if(itemLangRef.current != null && menuFloatRef.current != null){
+                    const rect = itemLangRef.current.getBoundingClientRect()
+                    const menu = menuFloatRef.current;
+                    menu.style.top = rect.y + "px"
+                }
+                
+            }
+            return !open;
+        });
     }
 
     const selectLang = (lang: string) => {
@@ -36,8 +49,14 @@ function NavegatorComponent({ preference, translationLiteral, changeLang, change
     }
 
     let showMenuDesktop = () => {
+        document.body.classList.remove("no-scroll");
         if(window.innerWidth < 768){
-            return openMenu?"move-right":"move-left";
+            if(openMenu) {
+                document.body.classList.add("no-scroll");
+                return "move-right";
+            }
+
+            return "move-left";
         }
         return "";
     }
@@ -51,14 +70,14 @@ function NavegatorComponent({ preference, translationLiteral, changeLang, change
                     </a>
                 </div>
             </nav>
-            <nav className={`navegator fixed py-4 h-dvh ${showMenuDesktop()}`}>
-                <div className='flex flex-col h-full'>
+            <nav className={`navegator fixed max-h-dvh  md:h-dvh ${showMenuDesktop()}`}>
+                <div className='flex flex-col h-full pt-4 pb-10 md:py-4'>
                     <div className='flex-none text-center text-2xl md:hidden mb-3'>
                         <a onClick={toggleMenu}>
                             <FontAwesomeIcon icon={faTimes} />
                         </a>
                     </div>
-                    <div className='flex-auto grid content-between'>
+                    <div className='flex-auto grid content-between '>
                         <ul className="px-1 md:px-3 grid gap-5">
 
                             <li>
@@ -112,28 +131,14 @@ function NavegatorComponent({ preference, translationLiteral, changeLang, change
                                 </a>
                             </li>
                         </ul>
-                        <ul className="px-1 md:px-3 relative grid gap-2 my-5">
-                            <li>
+
+                        <ul className="px-1 mt-10 md:mt-0 md:px-3 grid gap-2">
+                            <li ref={itemLangRef}>
                                 <a className={openLang ? '--active' : ''} onClick={toggleLang}>
                                     <div className='title--lang'>
                                         {lang?.toUpperCase() || "N/A"}
                                     </div>
                                 </a>
-                                {
-                                    openLang &&
-                                    <ul className='absolute lang gap-1 py-2 '>
-                                        <li>
-                                            <a className={`font-normal ${preference?.lang == 'en' ? '--active' : ''}`} onClick={() => selectLang('en')}>
-                                                EN (Enlgish)
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a className={`font-normal ${preference?.lang == 'es' ? '--active' : ''}`} onClick={() => selectLang('es')}>
-                                                ES (Español)
-                                            </a>
-                                        </li>
-                                    </ul>
-                                }
 
                             </li>
                             <li>
@@ -159,6 +164,23 @@ function NavegatorComponent({ preference, translationLiteral, changeLang, change
                 </div>
 
             </nav>
+            <div className='absolute' ref={menuFloatRef}> 
+                {
+                    openLang &&
+                    <ul className='absolute lang gap-1 py-2 '>
+                        <li>
+                            <a className={`font-normal ${preference?.lang == 'en' ? '--active' : ''}`} onClick={() => selectLang('en')}>
+                                EN (Enlgish)
+                            </a>
+                        </li>
+                        <li>
+                            <a className={`font-normal ${preference?.lang == 'es' ? '--active' : ''}`} onClick={() => selectLang('es')}>
+                                ES (Español)
+                            </a>
+                        </li>
+                    </ul>
+                }
+            </div>
         </>
     );
 }
